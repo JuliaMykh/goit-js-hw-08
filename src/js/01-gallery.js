79eea53e7fee15784e2c7efd49a7d90dfe93fc66
Mykhailenko <juliam4433@gmail.com>
2022-07-24 16:52:49 +0200

    update file-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryImg = createGallery(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryImg);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createGallery(galleryItems){
+    return galleryItems.map(({preview, original, description}) => {
+      return  `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+    }).join('');
+}
+
+function onGalleryContainerClick(e){
+  e.preventDefault();
+
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+    };
+    
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+  
+};
