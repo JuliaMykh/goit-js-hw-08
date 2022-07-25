7e957fbe9804fe6f10bbdfa21f118c59c570bbbf
Mykhailenko <juliam4433@gmail.com>
2022-07-25 16:47:26 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,7 +3,6 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('.feedback-form input');
 const message = document.querySelector('.feedback-form textarea');
-// const button = document.querySelector('.feedback-form button');
 
 form.addEventListener('input', throttle(onInputForm, 1000));
 form.addEventListener('submit', onSubmitForm);
@@ -14,22 +13,23 @@ const formData = {};
 const STORAGE_KEY = 'feedback-message';
 
 function onInputForm(e) {
-    formData[e.target.name] = e.target.name;
+    formData[e.target.name] = e.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function onSubmitForm(e) {
-    // console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
     e.preventDefault();
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
 }
 
 function saveTextArea() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
     if (savedMessage) {
-        email.value = data.email;
-        message.value = data.message;
+        email.value = savedMessage.email;
+        message.value = savedMessage.message;
     }
 };
+
 
